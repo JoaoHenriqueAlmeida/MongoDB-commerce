33a3b1b7553f708e034d005b36c7ca3f2fca3aeb
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-04-10 14:26:47 -0300

    fix: req 25 & 26 percentuals

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
-  { valoresNutricionais: { $elemMatch: { tipo: "sódio" } } },
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
   { $push: { tags: "muito sódio" } },
 );
 
